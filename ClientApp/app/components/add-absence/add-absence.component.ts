f3510dffdacc8f2a3d7cd0673963ef27197ef950
Katrina Ortiz <kortiz@instechnologies.net>
2017-06-29 12:05:34 -0400

    Add success message and refactor error messages in absence component

--- ClientApp/app/components/add-absence/add-absence.component.ts
+++ ClientApp/app/components/add-absence/add-absence.component.ts
@@ -10,42 +10,54 @@ declare var moment: any;
 })
 export class AddAbsenceComponent {
     public hasError: boolean = false;
+    public isSuccessful: boolean = false;
     public errorMessage: string = "";
     private tomorrow: Date = moment(new Date()).add('days', 1);
     @Input() absence: Absence = new Absence(this.tomorrow, this.tomorrow, "", "");
 
     public onAbsenceSubmit() {
         this.clearErrors();
+        this.isSuccessful = false;
 
         if (this.absence.name.trim() === "") {
-            this.errorMessage = "Name cannot be blank.";
-            this.hasError = true;
+            this.setErrorMessage("Name cannot be blank.");
             return;
         }
 
         if (this.absence.fromDate < moment().add('days', 1)) {
-            this.errorMessage = "From date must be in the future";
-            this.hasError = true;
+            this.setErrorMessage("From date must be in the future");
             return;
         }
 
         if (this.absence.toDate < this.absence.fromDate) {
-            this.errorMessage = "To date must be later than from date.";
-            this.hasError = true;
+            this.setErrorMessage("To date must be later than from date.");
             return;
         }
 
         this.clearForm();
+        this.setSuccess();
     }
 
-    private clearForm() {
+    private clearForm(): void {
         this.absence.fromDate = void 0;
         this.absence.toDate = void 0;
         this.absence.name = "";
         this.absence.comments = "";
     }
 
-    private clearErrors() {
+    private setErrorMessage(message: string): void {
+        this.errorMessage = message;
+        this.hasError = true;
+    }
+
+    private setSuccess(): void {
+        this.isSuccessful = true;
+        setTimeout(() => {
+            this.isSuccessful = false;
+        }, 10000);
+    }
+
+    private clearErrors(): void {
         this.errorMessage = "";
         this.hasError = false;
     }
