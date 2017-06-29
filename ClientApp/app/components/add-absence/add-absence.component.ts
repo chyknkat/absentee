67cdbf7a961d61ba2d144c44485459e81c895a1f
Katrina Ortiz <kortiz@instechnologies.net>
2017-06-29 11:41:13 -0400

    Add error checking for dates in add-absence component on submit

--- ClientApp/app/components/add-absence/add-absence.component.ts
+++ ClientApp/app/components/add-absence/add-absence.component.ts
@@ -1,5 +1,7 @@
 ﻿import { Component, Input } from '@angular/core';
 import { Absence } from "../../absence";
+import * as moment from 'moment';
+declare var moment: any;
 
 @Component({
     selector: 'add-absence',
@@ -9,9 +11,10 @@ import { Absence } from "../../absence";
 export class AddAbsenceComponent {
     public hasError: boolean = false;
     public errorMessage: string = "";
-    @Input() absence: Absence = new Absence(new Date(), new Date(), "", "");
+    private tomorrow: Date = moment(new Date()).add('days', 1);
+    @Input() absence: Absence = new Absence(this.tomorrow, this.tomorrow, "", "");
 
-   public onAbsenceSubmit() {
+    public onAbsenceSubmit() {
         this.clearErrors();
 
         if (this.absence.name.trim() === "") {
@@ -20,12 +23,24 @@ export class AddAbsenceComponent {
             return;
         }
 
+        if (this.absence.fromDate < moment().add('days', 1)) {
+            this.errorMessage = "From date must be in the future";
+            this.hasError = true;
+            return;
+        }
+
+        if (this.absence.toDate < this.absence.fromDate) {
+            this.errorMessage = "To date must be later than from date.";
+            this.hasError = true;
+            return;
+        }
+
         this.clearForm();
     }
 
     private clearForm() {
-        this.absence.fromDate = new Date();
-        this.absence.toDate = new Date();
+        this.absence.fromDate = void 0;
+        this.absence.toDate = void 0;
         this.absence.name = "";
         this.absence.comments = "";
     }
