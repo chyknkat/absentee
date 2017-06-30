dd7efbc445ec6db7b4297051a90d2ba943d6e73e
Katrina Ortiz <kortiz@instechnologies.net>
2017-06-30 11:32:03 -0400

    Add User and Absence tables and publish to local db

---
+++ Absentee.Database/Absence.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Absentee].[Absence]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] INT NOT NULL, 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [Comments] VARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_Absence_ToUser] FOREIGN KEY ([UserId]) REFERENCES [Absentee].[User]([Id])
+)
---
+++ Absentee.Database/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Absentee].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [FullName] VARCHAR(100) NULL, 
+    [Team] VARCHAR(50) NULL 
+)
