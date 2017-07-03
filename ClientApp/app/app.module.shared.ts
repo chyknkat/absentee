4c457e0d37cd747474b405050e80882afa6b66b9
Katrina Ortiz <kortiz@instechnologies.net>
2017-07-03 13:08:16 -0400

    Add routes for components

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -6,12 +6,13 @@ import * as jQuery from 'jquery';
 import { DatepickerModule } from 'ngx-bootstrap';
 import { ScheduleModule } from 'primeng/primeng';
 
-
 import { AppComponent } from './components/app/app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { AddAbsenceComponent } from './components/add-absence/add-absence.component';
+
 (window as any).jQuery = (window as any).$ = jQuery;
+
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
@@ -22,9 +23,10 @@ export const sharedConfig: NgModule = {
     ],
     imports: [
         RouterModule.forRoot([
-            { path: '', redirectTo: 'nav', pathMatch: 'full' },
-            { path: 'nav', component: NavComponent },
-            { path: '**', redirectTo: 'nav' }
+            { path: '', redirectTo: 'calendar', pathMatch: 'full' },
+            { path: 'calendar', component: CalendarComponent },
+            { path: 'addabsence', component: AddAbsenceComponent },
+            { path: '**', redirectTo: 'calendar' }
         ]),
         FormsModule,
         DatepickerModule.forRoot(),
--- ClientApp/app/components/nav/nav.component.ts
+++ ClientApp/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavItem } from "../../nav-item";
 
 @Component({
@@ -9,8 +10,16 @@ import { NavItem } from "../../nav-item";
 export class NavComponent {
     navItems: NavItem[] = [new NavItem("Calendar", true), new NavItem("Add Absence", false)];
 
+    constructor(private router: Router) { }
+
     onSelectNav(nav: NavItem) {
         this.navItems.forEach((navItem) => navItem.isActive = false);
         nav.isActive = true;
+        if (nav.name === 'Calendar') {
+            this.router.navigate(['calendar']);
+        }
+        if (nav.name === 'Add Absence') {
+            this.router.navigate(['addabsence']);
+        }
     }
 }
